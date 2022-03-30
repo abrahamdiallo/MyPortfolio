a916117f18a4a5fd4bc7e8a3b11997a80c9b6775
Abraham Diallo <abrahamdiallo1@gmail.com>
2022-03-30 22:46:03 +0000

    first commit

---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const projects = [
+  {
+    title: "Windows Servers",
+    subtitle: "",
+    description:
+      "This category takes you to my windows server project",
+    image: "./server.jpeg",
+    link: "https://web.facebook.com/watch/360297650791697/700308097661247/",
+  },
+  {
+    title: "Computer Networking",
+    subtitle: "Coming Soon!",
+    description:
+      "This category takes you to my networking project",
+    image: "./networks.jpg",
+    link: "https://example.com",
+  },
+  {
+    title: "I.T Support",
+    subtitle: "Coming Soon!",
+    description:
+      "This category takes you to my compTIA A+ videos ",
+    image: "./itsupport.jpg",
+    link: "https://example.com",
+  },
+  {
+    title: "MySql",
+    subtitle: "Coming Soon!",
+    description:
+      "This category takes you to MySql projects",
+    image: "./mysql.jpg",
+    link: "https://example.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "ABRAHAM",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "SAMUEL",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Windows Server",
+  "SQL Database",
+  "Network Administration",
+  "Server Administration",
+  "Software Installation",
+  "Hardware Installation",
+  "IT Service Management",
+  "Active Directory",
+  "Virtualization",
+  "Vmware ESXi",
+  "Vmware Workstation Pro",
+  "Hyper-v",
+  "Teamwork",
+];
