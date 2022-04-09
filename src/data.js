ae3afe5a07d4abc414421cf0058ad8fff10dec44
Abraham Diallo <80166325+abrahamdiallo@users.noreply.github.com>
2022-04-09 00:37:58 +0000

    Add files via upload

--- src/data.js
+++ src/data.js
@@ -36,17 +36,31 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "ABRAHAM",
-    company: "Zoozle",
+      "We meet people every day, but only some leave the marks, and one of them is Abraham",
+    image: "ibrahim.png",
+    name: "Ibrahim Mama Kouma",
+    company: "ADmyBRAND",
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "SAMUEL",
-    company: "Dooble.io",
+      "Abraham is a bright, motivated student who can stand his ground with students with more years and experience. His maturity and personality match his wit, allowing him to easily collaborate with peers and colleagues outside the university. It is my pleasure to have him in my class, and I look forward to watching him succeed in future endeavors.",
+    image: "overs.jpg",
+    name: "Samuel Overs Pelham",
+    company: "Weifang University",
+  },
+  {
+    quote:
+      "I can recommend Abraham Diallo as a person with deep knowledge and abilities in System Administration. A wise, clever, amazing, and well-educated person who can be trusted. Loyal, insightful, and independent. He is not only a reliable and forward-thinking student but also an inspiring team player. Abraham Diallo will find a way to weather any storm with a smile. He is a truly extraordinary individual. ",
+    image: "askia.jpg",
+    name: "Mohammed Jalloh",
+    company: "University of Liberia",
+  },
+  {
+    quote:
+      "I am constantly in awe of Abraham's grit and determination, as well as his ability to positively deal with challenging experiences. He is hardworking and driven and can accomplish anything he sets his mind to - he gets it done! This is why I look forward to collaborating more with him in the future and hope we can work together again.",
+    image: "mamudu.jpg",
+    name: "Alpha Mamudu Diallo",
+    company: "A.M.E Zion University",
   },
 ];
 
